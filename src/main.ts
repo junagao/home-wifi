09371bc4c4d1d8df3468985e20ba2da310946804
Juliane Nagao <juliane@hey.com>
2024-03-11 10:10:10 +0100

    add main menu and toggle wifi

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {Menu, Tray, app, type MenuItemConstructorOptions} from 'electron'
 import path from 'path'
 
 import {ROUTER_WIFI_URL} from './constants.ts'
-import {initialMenu} from './menu.ts'
+import {getMenu, initialMenu, updateMenuItemIcon} from './menu.ts'
 import {
   __dirname,
   aboutPanelOptions,
@@ -10,6 +10,7 @@ import {
   getBrowser,
   getTrayIcon,
   login,
+  sleep,
   startLoadingAnimation,
 } from './utils.ts'
 
@@ -39,4 +40,53 @@ app.on('ready', async () => {
   tray.setImage(trayIcon)
 
   setTimeout(() => clearInterval(loadingInterval))
+
+  menu = getMenu({browser, tray, wifi24GhzStatus, wifi5GhzStatus})
+
+  const contextMenu = Menu.buildFromTemplate(menu)
+  tray.setContextMenu(contextMenu)
+  await page.close()
+  await browser.close()
 })
+
+export const toggleWifi = async ({selector, tray}: {selector: string; tray: Tray}) => {
+  const loadingInterval = startLoadingAnimation({tray})
+
+  const browser = await getBrowser()
+
+  const page = await browser.newPage()
+
+  await login(page)
+
+  await page.goto(ROUTER_WIFI_URL, {waitUntil: 'domcontentloaded'})
+
+  await page.waitForSelector(selector, {visible: true})
+  await page.click(selector)
+
+  const applyButtonSelector = `button[translate="apply"]`
+  await page.waitForSelector(applyButtonSelector)
+  await page.click(applyButtonSelector)
+  console.log(`Please wait...`)
+
+  await sleep(1)
+
+  await page.keyboard.press('Tab')
+  await page.keyboard.press('Tab')
+  await page.keyboard.press('Enter')
+
+  await page.waitForSelector(`div[translate="pleaseWaitLoading"]`, {hidden: true})
+
+  const {wifi24GhzStatus, wifi5GhzStatus} = await checkWifiStatus(page)
+  const trayIcon = getTrayIcon(wifi24GhzStatus, wifi5GhzStatus)
+  tray.setImage(trayIcon)
+
+  setTimeout(() => clearInterval(loadingInterval))
+
+  menu[0].icon = updateMenuItemIcon(wifi24GhzStatus)
+  menu[1].icon = updateMenuItemIcon(wifi5GhzStatus)
+  const contextMenu = Menu.buildFromTemplate(menu)
+  tray.setContextMenu(contextMenu)
+
+  await page.close()
+  await browser.close()
+}
--- src/menu.ts
+++ src/menu.ts
@@ -1,6 +1,10 @@
-import {app, shell, type MenuItemConstructorOptions} from 'electron'
+import {app, shell, type MenuItemConstructorOptions, type Tray} from 'electron'
+import path from 'path'
+import {type Browser} from 'puppeteer-core'
 
 import {ROUTER_URL} from './constants.ts'
+import {toggleWifi} from './main.ts'
+import {__dirname} from './utils.ts'
 
 const about: MenuItemConstructorOptions = {
   label: 'About',
@@ -36,4 +40,59 @@ export const initialMenu: MenuItemConstructorOptions[] = [
   separator,
   about,
   {...quit, click: () => app.quit()},
+]
+
+type Status = 'UP' | 'DOWN'
+
+export const updateMenuItemIcon = (status: Status) => {
+  return status === 'UP'
+    ? path.join(__dirname, '/icons/wifi-up.png')
+    : path.join(__dirname, '/icons/wifi-down.png')
+}
+
+type GetMenuProps = {
+  browser: Browser
+  tray: Tray
+  wifi24GhzStatus: Status
+  wifi5GhzStatus: Status
+}
+
+export const getMenu = ({
+  browser,
+  tray,
+  wifi24GhzStatus,
+  wifi5GhzStatus,
+}: GetMenuProps): MenuItemConstructorOptions[] => [
+  {
+    label: 'Wifi 2.4Ghz',
+    icon: updateMenuItemIcon(wifi24GhzStatus),
+    click: async () => {
+      await browser.close()
+      await toggleWifi({
+        selector: `input[id="enable-wifi-24"]`,
+        tray,
+      })
+    },
+  },
+  {
+    label: 'Wifi 5Ghz',
+    icon: updateMenuItemIcon(wifi5GhzStatus),
+    click: async () => {
+      await toggleWifi({
+        selector: `input[id="enable-wifi-5"]`,
+        tray,
+      })
+    },
+  },
+  separator,
+  routerPage,
+  separator,
+  about,
+  {
+    ...quit,
+    click: async () => {
+      await browser.close()
+      app.quit()
+    },
+  },
 ]
